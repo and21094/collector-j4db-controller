56c19b59c676005ba094cdae0ee77522605a9d8e
estebancas <estcascor94@hotmail.com>
2018-12-12 19:30:27 -0600

    first commit, split proccess

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = require('./lib/db')
+
+// TEST
+// const db = require('./lib/db')
+
+// const user = db.user.save({
+//     name: 'esteban',
+//     email: 'estcas@gmail.com',
+//     phone: '83636383'
+// })
+
+// console.log('response', user)
